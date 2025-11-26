6d12b635f484089fbea11183cb1d1243638bccd3
kristina378 <kristinadubiaha@gmail.com>
2025-11-26 15:35:18 +0100

    Resolved merge conflikt

--- program.cpp
+++ program.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 
+bool nieujemna(int n){
+    return n>=0;
+}
+
+
 long long silnia(long n){
     long long silnia = 1;
     for(unsigned i = 2; i <= n;i++){
@@ -14,6 +19,12 @@ int main(void){
     int n;
 
     std::cin>>n;
+
+    if(!nieujemna(n)){
+        std::cerr<<"Liczba jest ujemna";
+        return 1;
+    }
+    
     std::cout<<silnia(n);
 
     return 0;
