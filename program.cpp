77368af696f307cc789df0b4f10118447fed068f
kristina378 <kristinadubiaha@gmail.com>
2025-11-26 15:17:21 +0100

    Add factorial functionality

--- program.cpp
+++ program.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 
+long long silnia(long n){
+    long long silnia = 1;
+    for(unsigned i = 2; i <= n;i++){
+        silnia*=i;
+    }
+    return silnia;
+}
+
+
 int main(void){
-    
+
     int n;
 
     std::cin>>n;
-    std::cout<<n;
+    std::cout<<silnia(n);
 
     return 0;
 }
