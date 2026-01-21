489891f613aed2d5ff120263cc77d531bd77d627
kristina378 <kristinadubiaha@gmail.com>
2026-01-21 16:04:33 +0100

    Add factorial functionality

--- program.cpp
+++ program.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 
+unsigned long long silnia(int n){
+    if(n < 0){
+        std::cerr << "Cant count factorial of negative number\n";
+        return -1;
+    }
+
+    unsigned long long silnia = 1;
+    for(unsigned i = 2; i <= n; i++){
+        silnia *= i;
+    }
+
+    return silnia;
+}
+
 int main(void){
     
     int n;
 
     std::cin>>n;
-    std::cout<<n;
+    std::cout << silnia(n) << std::endl;
 
     return 0;
 }
