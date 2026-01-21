172478f1780fe1a764adea40d018f119abd53f3d
kristina378 <kristinadubiaha@gmail.com>
2026-01-21 22:42:40 +0100

    Add check if num is negative

--- program.cpp
+++ program.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 
 int main(void){
-    
     int n;
 
     std::cin>>n;
-    std::cout<<n;
+    if(n < 0){
+        std::cout<<"Liczba ujemna\n";
+        return 0;
+    }
+
+    std::cout << n;
 
     return 0;
 }
